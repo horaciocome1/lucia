6008c17d01b6295ee1547d9b6be7787b73ae58a4
Horácio Comé <hcome@pm.me>
2022-09-24 22:42:32 +0200

    - remove deprecated code

--- app/src/main/java/io/github/horaciocome1/lucia/setup/ui/SetupScaffold.kt
+++ app/src/main/java/io/github/horaciocome1/lucia/setup/ui/SetupScaffold.kt
@@ -17,8 +17,8 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -43,7 +43,7 @@ fun SetupScaffold(
     Scaffold(
         modifier = modifier,
         topBar = {
-            SmallTopAppBar(
+            TopAppBar(
                 title = {},
                 navigationIcon = {
                     if (!firstStep) {
